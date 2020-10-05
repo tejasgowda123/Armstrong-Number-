5f8e687afdfa40476d0f969248741851cbd4b5fc
Tejas gowda <47363039+tejasgowda123@users.noreply.github.com>
2020-10-05 18:43:53 +0100

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -2,9 +2,11 @@ class ArmstrongExample{
 
   public static void main(String[] args)  {  
 
-    int c=0,a,temp;  
+    int c=0;
+    int a;
+    int temp;  
 
-    int n=153;//It is the number to check armstrong  
+    int n=153;   //This is the number to check armstrong  
 
     temp=n;  
 
@@ -21,12 +23,12 @@ class ArmstrongExample{
     }  
 
     if(temp==c)  
-
-    System.out.println("armstrong number");   
-
+    
+    System.out.println("It is a armstrong number");   
+  
     else  
 
-        System.out.println("Not armstrong number");   
+        System.out.println("It is Not armstrong number");   
 
    }  
 
